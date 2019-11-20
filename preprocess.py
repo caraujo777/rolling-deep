815d9421359730f4900e1e4e6d78a326fb0c7ece
Cintia Araujo <cintia_araujo@brown.edu>
2019-11-20 07:24:33 -0500

    Filter tweets: only republican and democratic now

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,33 @@
-import json
+import json
 
-file_path = "political_tweets.ndjson"
+file_path = "political_tweets.ndjson"
 
-# load from file-like objects
-data =[]
-with open(file_path) as f:
-    for line in f:
-        j_content = json.loads(line)
-        data.append(j_content)
-print(len(data))
-print(data[0])
+with open("democrat_users.txt", "r") as f:
+    for line in f:
+        dem = line.replace(" ", "").split(',')
+f.close()
+
+with open("republican_users.txt", "r") as f:
+    for line in f:
+        rep = line.replace(" ", "").split(',')
+f.close()
+
+with open("parsed_parties.txt", "w+") as f:
+    json.dump("hallo", f)
+
+data=[]
+with open(file_path) as f:
+    for line in f:
+        j_content = json.loads(line)
+        user = (j_content['user']['screen_name'])
+        if(user in dem):
+            j_content['user']['party'] = 'democratic'
+            data.append(j_content)
+        elif(user in rep):
+            j_content['user']['party'] = 'republican'
+            data.append(j_content)
+
+with open("parsed_parties.txt", "w+") as f:
+    for d in data:
+        json.dump(d, f)
+f.close()
